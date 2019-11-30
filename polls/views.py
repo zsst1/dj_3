9e41693ee6581aa3213bbad1c01ba7bd43383dc9
zsst1 <zs2368@columbia.edu>
2019-11-30 01:38:29 +0000

    django tutorial part 1_3  with '#' will be ignored, and an empty message aborts the commit.

---
+++ polls/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.template import loader
+from .models import Question
+from django.http import HttpResponse
+
+def index(request):
+    latest_question_list =  Question.objects.order_by('-pub_date')[:5]
+   ##template = loader.get_template('polls/index.html')
+    context = {
+            'latest_question_list': latest_question_list
+            }
+    #output = ' , '.join(q.question_text for q in latest_question_list])
+   ## return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse('You are looking at question %s.' %question_id)
+
+
+def results(request,question_id):
+    response = 'You are looking at the results of question %s.'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('You are voting on quetion %s.' %question_id)
+
+
+
